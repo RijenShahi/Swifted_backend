e4faf9b9c583f8fcd8be9b527cba7d081cf7b765
hrize <hrizexshahi@gmail.com>
2021-07-27 08:35:23 +0545

    Model for ordering is created.

---
+++ models/order.modals.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  additionalInfo: {
+    type: String,
+  },
+  paymentMethod: {
+    type: String,
+    enum: ["Cash On Delivery, Credit/Debit, E-Sewa/Khalti"],
+    required: true,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
